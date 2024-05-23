3bb06c65829ed73e60dd2b741e889a9cb2917772
Sopbaldini2 <sop.baldini@stud.uniroma3.it>
2024-05-23 12:32:33 +0200

    Implementazione delle classi legate alla sicurezza

---
+++ siw-Food-03/src/main/java/it/uniroma3/siw/model/Ingredient.java
@@ -0,0 +1,82 @@
+package it.uniroma3.siw.model;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+
+@Entity
+public class Ingredient {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long idIngredient;
+	private String name;
+	private Float quantita;
+	@ManyToMany(mappedBy="ingredients")
+	private Set<Recipe> recipes;
+	
+	public Ingredient() {
+		this.recipes = new HashSet<>();
+	}
+
+	//Aggiungo i metodi Getter e Setter
+	
+	public Long getIdIngredient() {
+		return idIngredient;
+	}
+
+	public void setIdIngredient(Long idIngredient) {
+		this.idIngredient = idIngredient;
+	}	
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Float getQuantita() {
+		return quantita;
+	}
+
+	public void setQuantita(Float quantita) {
+		this.quantita = quantita;
+	}
+
+	public Set<Recipe> getRecipes() {
+		return recipes;
+	}
+
+	public void setRecipes(Set<Recipe> recipes) {
+		this.recipes = recipes;
+	}
+
+	//Aggiungo i metodi Equals() e HashCode() 
+	//Due oggetti Recipe sono uguali se hanno lo stesso id, lo stesso nome e la stessa ricetta
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(recipes, name, quantita);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Ingredient other = (Ingredient) obj;
+		return Objects.equals(recipes, other.recipes) && Objects.equals(name, other.name)
+				&& Objects.equals(quantita, other.quantita);
+	}
+}
