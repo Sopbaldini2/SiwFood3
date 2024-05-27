8d884d2c622142be0a3eb4d0526ad1862942c078
Sopbaldini2 <sop.baldini@stud.uniroma3.it>
2024-05-27 23:54:49 +0200

    Modifica ma bisogna capire bene il funzionamento

--- siw-Food-03/src/main/resources/import.sql
+++ siw-Food-03/src/main/resources/import.sql
@@ -5,10 +5,10 @@ INSERT INTO ingredient (id, name, quantita, description, image) VALUES(nextval('
 INSERT INTO ingredient (id, name, quantita, description, image) VALUES(nextval('ingredient_seq'), 'Timo', 150, 'Erba aromatica con un sapore forte e pungente.', 'timo.png');
 INSERT INTO ingredient (id, name, quantita, description, image) VALUES(nextval('ingredient_seq'), 'Origano', 50, 'Erba mediterranea usata essenzialmente nella cucina italiana.', 'origano.png');
 
-INSERT INTO users (id, name, surname, email, date_of_birth) VALUES (1, 'Gordon', 'Ramsay', 'gordon@example.com', '1966-11-08');
-INSERT INTO users (id, name, surname, email, date_of_birth) VALUES (2, 'Jamie', 'Oliver', 'jamie@example.com', '1975-05-27');
+INSERT INTO users (id, name, surname, email, date_of_birth) VALUES (nextval('users_seq'), 'Gordon', 'Ramsay', 'gordon@example.com', '1966-11-08');
+INSERT INTO users (id, name, surname, email, date_of_birth) VALUES (nextval('users_seq'), 'Jamie', 'Oliver', 'jamie@example.com', '1975-05-27');
 
 INSERT INTO recipe (id, name, description, image, cooke_id) VALUES (nextval('recipe_seq'), 'Cachupa', 'A fresh salad with tomatoes and basil.', 'Cachupa.png', 1);
-INSERT INTO recipe (id, name, description, image, cooke_id) VALUES (nextval('recipe_seq'), 'Gumbo', 'Classic Italian pasta with tomato sauce.', 'gumbo.png', 2);
+INSERT INTO recipe (id, name, description, image, cooke_id) VALUES (nextval('recipe_seq'), 'Gumbo', 'Classic Italian pasta with tomato sauce.', 'gumbo.png', 51);
 
 
