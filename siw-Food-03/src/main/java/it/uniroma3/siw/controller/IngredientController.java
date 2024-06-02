0c447c5b7945fd9c4996a9866a264bed4c9764a6
Sopbaldini2 <sop.baldini@stud.uniroma3.it>
2024-06-02 12:49:11 +0200

    Implementazione del fiel formNewIngredients e relativo css

--- siw-Food-03/src/main/java/it/uniroma3/siw/controller/IngredientController.java
+++ siw-Food-03/src/main/java/it/uniroma3/siw/controller/IngredientController.java
@@ -1,16 +1,19 @@
 package it.uniroma3.siw.controller;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
 
 import it.uniroma3.siw.model.Ingredient;
 import it.uniroma3.siw.service.IngredientService;
@@ -33,7 +36,7 @@ public class IngredientController {
 			model.addAttribute("ingredient", ingredient);
 			return "ingredient.html";
 		} else {
-			model.addAttribute("messaggioErrore", "Questo artista esiste già");
+			model.addAttribute("messaggioErrore", "Questo ingrediente esiste già");
 			return "usAd/formNewIngredient.html"; 
 		}
 	}
@@ -88,5 +91,4 @@ public class IngredientController {
             return "error";
         }
     }
-	
 }
